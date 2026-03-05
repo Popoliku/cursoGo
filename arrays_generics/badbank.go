21d78059f87b55dcee70659f8a059273c6b6dcff
Popoliku <andres1239cl@gmail.com>
2026-03-05 10:06:35 +0100

    arrays with generics

---
+++ arrays_generics/badbank.go
@@ -0,0 +1,30 @@
+package arrays_generics
+
+type Transaction struct {
+	From string
+	To   string
+	Sum  float64
+}
+
+type Account struct {
+	Name    string
+	Balance float64
+}
+
+func NewTransaction(from, to Account, sum float64) Transaction {
+	return Transaction{From: from.Name, To: to.Name, Sum: sum}
+}
+
+func NewBalanceFor(account Account, transactions []Transaction) Account {
+	return Reduce(transactions, applyTransaction, account)
+}
+
+func applyTransaction(account Account, transaction Transaction) Account {
+	if transaction.From == account.Name {
+		account.Balance -= transaction.Sum
+	}
+	if transaction.To == account.Name {
+		account.Balance += transaction.Sum
+	}
+	return account
+}
---
+++ arrays_generics/sum.go
@@ -0,0 +1,38 @@
+package arrays_generics
+
+func Reduce[A any, B any](collection []A, f func(B, A) B, initialValue B) B {
+	var result = initialValue
+	for _, x := range collection {
+		result = f(result, x)
+
+	}
+	return result
+}
+
+func Sum(numbers []int) int {
+	add := func(acc, y int) int {
+		return acc + y
+	}
+	return Reduce(numbers, add, 0)
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
+
+func SumAllTails(numbers ...[]int) []int {
+	sumTail := func(acc, x []int) []int {
+		if len(x) == 0 {
+			return append(acc, 0)
+		} else {
+			tail := x[1:]
+			return append(acc, Sum(tail))
+		}
+	}
+
+	return Reduce(numbers, sumTail, []int{})
+}
